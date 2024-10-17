14e5b6e0738494b4401fedf1cac802c0340d99f0
ducclucc <78633202+ducclucc@users.noreply.github.com>
2024-10-18 09:51:03 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from flask import Flask, request
-
+import requests
+def get_public_ip():
+    try:
+        response = requests.get('https://api.ipify.org?format=json')
+        ip_address = response.json()['ip']
+        return ip_address
+    except:
+        pass
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
@@ -14,4 +21,5 @@ def upload_file():
     return 'File saved successfully', 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    print(get_public_ip())
+    app.run(host=input('enter your host ip: '), port=5000)
