5b183ac97e257b57ae2100b891433bcaeb439745
ducclucc <78633202+ducclucc@users.noreply.github.com>
2024-10-18 09:58:55 +1100

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def upload_file():
     return 'File saved successfully', 200
 
 if __name__ == '__main__':
-    host = os.getenv('HOST', '0.0.0.0')  # Use the HOST environment variable or default to '0.0.0.0'
-    port = int(os.getenv('PORT', 5000))   # Use the PORT environment variable or default to 5000
+    host = '0.0.0.0'  # Use the HOST environment variable or default to '0.0.0.0'
+    port = 10000   # Use the PORT environment variable or default to 5000
     app.run(host=host, port=port)
