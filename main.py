f5b653bcba94953d91b98ee7e21e383c468b4bfb
ducclucc <78633202+ducclucc@users.noreply.github.com>
2024-10-18 09:54:02 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
+import os
 from flask import Flask, request
-import requests
-def get_public_ip():
-    try:
-        response = requests.get('https://api.ipify.org?format=json')
-        ip_address = response.json()['ip']
-        return ip_address
-    except:
-        pass
+
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def upload_file():
-    # Get the raw data from the request
     data = request.data
-
-    # Save the raw data to a file
     with open('uploaded_file', 'wb') as f:
         f.write(data)
-
     return 'File saved successfully', 200
 
 if __name__ == '__main__':
-    print(get_public_ip())
-    app.run(host=input('enter your host ip: '), port=5000)
+    host = os.getenv('HOST', '0.0.0.0')  # Use the HOST environment variable or default to '0.0.0.0'
+    port = int(os.getenv('PORT', 5000))   # Use the PORT environment variable or default to 5000
+    app.run(host=host, port=port)
